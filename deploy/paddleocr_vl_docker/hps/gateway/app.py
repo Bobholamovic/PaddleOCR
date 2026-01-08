57b8a1420d59293eb3a3f223f29f8c693d4c40c2
Bobholamovic <bob1998425@hotmail.com>
2026-01-08 02:48:09 +0000

    Enhance gateway server

--- deploy/paddleocr_vl_docker/hps/gateway/app.py
+++ deploy/paddleocr_vl_docker/hps/gateway/app.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# TODO:
+# 1. Concurrency control
+# 2. Timeout control
+# 3. Seperate infer and non-infer operations
+# 4. Fix FastAPI encoding bug
+
 import logging
 
 import fastapi
@@ -39,20 +45,76 @@ def _create_aistudio_output_without_result(error_code: str, error_msg: str) -> d
 def _add_primary_operations(app: fastapi.FastAPI) -> None:
     def _create_handler(model_name: str):
         def _handler(request: dict):
+            request_log_id = generate_log_id()
+            logger.info(
+                "Gateway server starts processing %r request %s",
+                model_name,
+                request_log_id,
+            )
+            if "logId" in request:
+                logger.warning(
+                    "Duplicate 'logId' field found in %r request %s",
+                    model_name,
+                    request_log_id,
+                )
+            request["logId"] = request_log_id
+
             try:
-                output = triton_request(triton_client, model_name, request)
+                output = triton_request(
+                    triton_client,
+                    model_name,
+                    request,
+                    request_kwargs=dict(
+                        timeout=600,
+                        client_timeout=600,
+                    ),
+                )
+            except triton_grpc.InferenceServerException as e:
+                if e.message() == "Deadline Exceeded":
+                    logger.warning(
+                        "Timeout when processing %r request %s",
+                        model_name,
+                        request_log_id,
+                    )
+                    is_timedout = True
+                    status_code = 504
+                    output = _create_aistudio_output_without_result(
+                        504, "Gateway timeout"
+                    )
+                    output = output.model_dump()
+                else:
+                    logger.error(
+                        "Failed to process %r request %s due to `InferenceServerException`: %s",
+                        model_name,
+                        request_log_id,
+                        e,
+                    )
+                    status_code = 500
+                    output = _create_aistudio_output_without_result(
+                        500, "Internal server error"
+                    )
+                    output = output.model_dump()
             except Exception as e:
-                logger.error("Failed to process request", exc_info=True)
+                logger.error(
+                    "Failed to process %r request %s",
+                    model_name,
+                    request_log_id,
+                    exc_info=True,
+                )
+                status_code = 500
                 output = _create_aistudio_output_without_result(
                     500, "Internal server error"
                 )
+                output = output.model_dump()
                 return JSONResponse(status_code=500, content=output)
             if output["errorCode"] != 0:
                 output = _create_aistudio_output_without_result(
                     output["errorCode"], output["errorMsg"]
                 )
-                return JSONResponse(status_code=output.errorCode, content=output)
-            return JSONResponse(status_code=200, content=output)
+                output = output.model_dump()
+            else:
+                status_code = 200
+            return JSONResponse(status_code=status_code, content=output)
 
         return _handler
 
@@ -78,7 +140,7 @@ def check_health():
         output = _create_aistudio_output_without_result(
             503, "The Triton server is not ready."
         )
-        return JSONResponse(status_code=503, content=output)
+        return JSONResponse(status_code=503, content=output.model_dump())
     return _create_aistudio_output_without_result(0, "Healthy")
 
 
@@ -94,6 +156,8 @@ class _EndpointFilter(logging.Filter):
 
 logging.getLogger("uvicorn.access").addFilter(_EndpointFilter())
 
+# HACK
 triton_client: triton_grpc.InferenceServerClient = triton_grpc.InferenceServerClient(
-    TRITONSERVER_URL
+    TRITONSERVER_URL,
+    keepalive_options=triton_grpc.KeepAliveOptions(keepalive_timeout_ms=600000),
 )
